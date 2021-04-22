d9355bb6f42c42f8e11dd83513da677e935ccd02
andijavist <grungeisnotdead1@yandex.ru>
2021-04-22 13:28:25 +0300

    new changes to NewClass.java

--- src/main/java/web/servlets/secondtime/NewClass.java
+++ src/main/java/web/servlets/secondtime/NewClass.java
@@ -3,5 +3,10 @@ package web.servlets.secondtime;
 public class NewClass extends GitTesting{
     public NewClass(int a, boolean b) {
         super(a, b);
+
+    }
+    static {
+        System.out.println("smth");
     }
 }
+
